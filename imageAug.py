9728712cc70cceac5707bf31b081aa32f0a29f10
ksehan89 <ksehan89@naver.com>
2019-09-09 18:28:39 +0900

    Update imageAug.py

--- imageAug.py
+++ imageAug.py
@@ -1,6 +1,7 @@
-import sys 
+import sys, os 
 from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit, QCheckBox, QPushButton, QToolButton, QSizePolicy, QFileDialog)
 from PyQt5.QtCore import Qt
+from PIL import Image, ImageOps
 
 
 class Button(QToolButton):
@@ -26,25 +27,27 @@ class MyApp(QWidget):
 		grid = QGridLayout()
 		self.setLayout(grid)
 
-#grid.addWidget(QLabel('Title:'), 0, 0)
+
 		# QCheckBox
 		self.cb1 = QCheckBox('Resize', self)
 		#cb1.toggle()
 		self.cb1.stateChanged.connect(self.checkBoxState)
+		#grid.addWidget(QLabel('Title:'), 0, 0)
 		grid.addWidget(self.cb1, 0, 0)
 
 		self.cb2 = QCheckBox('rotate', self)
 		self.cb2.stateChanged.connect(self.checkBoxState)
 		grid.addWidget(self.cb2, 1, 0)
 		
-		cb3 = QCheckBox('hflip', self)
-		grid.addWidget(cb3, 2, 0)
+		self.cb3 = QCheckBox('hflip', self)
+		grid.addWidget(self.cb3, 2, 0)
 
-		cb4 = QCheckBox('vflip', self)
-		grid.addWidget(cb4, 3, 0)
+		self.cb4 = QCheckBox('vflip', self)
+		grid.addWidget(self.cb4, 3, 0)
 		
-		cb5 = QCheckBox('rename', self)
-		grid.addWidget(cb5, 4, 0)
+		self.cb5 = QCheckBox('rename', self)
+		self.cb5.stateChanged.connect(self.checkBoxState)
+		grid.addWidget(self.cb5, 4, 0)
 
 
 		# QLineEdit
@@ -59,15 +62,18 @@ class MyApp(QWidget):
 		#rotate_edit.textChanged[str].connect(self.onChanged)
 		grid.addWidget(self.rotate_edit, 1, 1)
 
-		rename_prefix = QLineEdit(self)
+		self.rename_prefix = QLineEdit(self)
+		self.rename_prefix.setDisabled(True)
 		#rename_prefix.textChanged[str].connect(self.onChanged)
-		grid.addWidget(rename_prefix, 5, 1)
+		grid.addWidget(self.rename_prefix, 5, 1)
 
-		rename_suffix = QLineEdit(self)
+		self.rename_suffix = QLineEdit(self)
+		self.rename_suffix.setDisabled(True)
 		#rename_suffix.textChanged[str].connect(self.onChanged)
-		grid.addWidget(rename_suffix, 5, 3)
+		grid.addWidget(self.rename_suffix, 5, 3)
 	
-		self.path = QLineEdit(self)
+		#self.path = QLineEdit('/home/ben/사진/index.jpeg')
+		self.path = QLineEdit()
 		#path.textChanged[str].connect(self.onChanged)
 		grid.addWidget(self.path, 6, 1, 1, 2)
 
@@ -101,30 +107,34 @@ class MyApp(QWidget):
 		#self.btn2.clicked.connect(self.browseClicked)
 
 
-
-
-
 		self.setWindowTitle('ImageAug')
 		self.setGeometry(300, 200, 450, 250)
 		self.show()
 
+
 	def checkBoxState(self):
-		msg = ""
 		if self.cb1.isChecked():
-		#resize_edit.setEnabled(True)
 			self.resize_edit.setDisabled(False)
-		
+		else:
+			self.resize_edit.setDisabled(True)
+
 		if self.cb2.isChecked():
 			self.rotate_edit.setDisabled(False)
-		
-		if cb3.isChecked() == True:
-			hflip
-		
-		if cb4.isChecked() == True:
-			vflip
+		else:
+			self.rotate_edit.setDisabled(True)
 
-		if cb5.isChecked() == True:
-			rename_edit
+		#if cb3.isChecked() == True:
+			#hflip
+
+		#if cb4.isChecked() == True:
+			#vflip
+
+		if self.cb5.isChecked():
+			self.rename_prefix.setDisabled(False)
+			self.rename_suffix.setDisabled(False)
+		else:
+			self.rename_prefix.setDisabled(True)
+			self.rename_suffix.setDisabled(True)
 		return
 
 
@@ -133,12 +143,70 @@ class MyApp(QWidget):
 		button.clicked.connect(member)
 		return button
 
+
 	def runClicked(self):
+		print(self.path.text())
+		self.img = Image.open(self.path.text())
+		self.savePath = './ImageAgu/'
+	
+		fname = os.path.split(self.path.text())[1]
+		#fname = os.path.split('/home/ben/사진/index.jpeg')[1]
+		print(fname)
+
+		# 이미지 사이즈 조정 및 저장
+		if self.cb1.isChecked():
+			resize_size = self.resize_edit.text()
+			resize_arr = resize_size.split(',')
+			if len(resize_arr) == 1:
+				resize_arr.append(resize_arr[0])
+			self.img = self.img.resize((int(resize_arr[0]), int(resize_arr[1])))
+			fname = 'RS_' + fname
+			self.savePath = self.savePath + 'Resize_'
+
+		# 이미지 회전 및 저장
+		if self.cb2.isChecked():
+			self.img = self.img.rotate(int(self.rotate_edit.text()))
+			#print(self.img)
+			fname = 'RT_' + fname
+			#print(fname)
+			self.savePath = self.savePath + 'Rotate_'
+		
+		# 이미지 수평 대칭 및 저장
+		if self.cb3.isChecked():
+			self.img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
+			fname = 'HFLIP_' + fname
+			self.savePath = self.savePath + 'HFLIP_'
+
+		# 이미지 수직 대칭 및 저장
+		if self.cb4.isChecked():
+			self.img = self.img.transpose(Image.FLIP_LEFT_RIGHT)
+			fname = 'VFLIP_' + fname
+			self.savePath = self.savePath + 'VFLIP_'
+
+		# 이미지 리네임
+		if self.cb5.isChecked():
+			image_path = self.path.text()
+			image_ext = os.path.splitext(image_path)
+			print(image_ext)
+			
+			split_name_ext = fname.rsplit('.', 1)
+
+			fname = self.rename_prefix.text() + '_' + split_name_ext[0] + '_' + self.rename_suffix.text() + '.' + split_name_ext[1]
+			self.savePath = self.savePath + 'RENAME_'
+		
+		# 저장할 경로에 디렉토리가 없으면 생성
+		if not(os.path.isdir(self.savePath)):
+			os.makedirs(os.path.join(self.savePath))
+		
+		self.img.save(self.savePath + '/' + fname)
+		
 		return
+
 
 	def browseClicked(self):
 		fname = QFileDialog.getOpenFileName(self)
 		self.path.setText(fname[0])
+		fname = self.path.text()
 		return
 	
 	#def onChanged(self, text):
